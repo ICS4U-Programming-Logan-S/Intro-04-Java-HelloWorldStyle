db16627319b3760b59dd1a3437efda5f33dddd54
Logan Sweeney <logan.sweeney@stu.ocsb.ca>
2023-02-14 19:36:25 +0000

    Finished HelloWorldStyle

---
+++ HelloWorldStyle.java
@@ -0,0 +1,30 @@
+/**
+* Says Hello World.
+*
+* @author  Logan S
+* @version 1.0
+* @since   2023-02-14
+*/
+public final class HelloWorldStyle {
+
+    /**
+     * This is a private constructor.
+     *
+     */
+    private HelloWorldStyle() { }
+
+    /**
+     * This is the main method.
+     *
+     * @param args
+     *
+     */
+    public static void main(String[] args) {
+        // prints What's up buttercup
+        System.out.println("What's up buttercup");
+        // prints a sentence
+        System.out.print("Rodents of unusual size? ");
+        System.out.println("I don't think they exist.");
+        System.out.println("Or do they? maybe you should search it.");
+    }
+}
